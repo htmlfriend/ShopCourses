07fd78104e24b34e87f3f9ef11e5652d1470f898
yurii <yuriy.tim@yahoo.com>
2020-06-09 22:19:47 -0400

    connect to mongodb and changed models

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Handlebars = require("handlebars");
 const path = require("path");
+// fucking mongoose needs to resolve the property oh handelbars!!!
+// you need to install handlebars/allow-prototype-access
+// or try to install express-handlebars 4.5.0 or higher
 const exphbrs = require("express-handlebars");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
 const app = express();
 const hbs = exphbrs.create({
   defaultLayout: "main",
   extname: "hbs",
+
+  handlebars: allowInsecurePrototypeAccess(Handlebars),
 });
 
 const homeRouter = require("./routes/home");
@@ -26,6 +36,21 @@ app.use("/card", cardRouter);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, (req, res) => {
-  console.log("I am running ... on", PORT);
-});
+async function start() {
+  try {
+    const url = `mongodb+srv://user:password@cluster0-5y97q.mongodb.net/shop`;
+    // remove user and password
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    app.listen(PORT, (req, res) => {
+      console.log("I am running ... on", PORT);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
